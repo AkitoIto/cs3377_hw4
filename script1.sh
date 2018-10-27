3d1baabb3dc901b24229b2a2513a6b8c64cb11de
AkitoIto <akitoned@gmail.com>
2018-10-27 18:41:34 -0500

    modified script1

--- script1.sh
+++ script1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+#creating dir1 and file10 inside the dir1
 mkdir dir1 && touch dir1/file10
-mkdir dir2 && touch dir2/file2
+#creating dir2 and file20 inside the dir2
+mkdir dir2 && touch dir2/file20
+#creating file 1
 touch file1
+#soft link to file20
 ln -s dir2/file20 link1
