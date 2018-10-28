144aea152ddc7d8fc73229c041b2d90e6312bf93
AkitoIto <akitoned@gmail.com>
2018-10-27 21:01:51 -0500

    myprog2.c added

---
+++ myprog2.c
@@ -0,0 +1,13 @@
+/*This is a c program that creates directories, files, and a link using function calls*/
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char * argv[])
+{
+  /*creatin dir1*/
+
+  
+
+
+   return 0;
+}
