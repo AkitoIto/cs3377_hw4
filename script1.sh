0b30325859a2f7f72a8481c581a3e4310a687b41
AkitoIto <akitoned@gmail.com>
2018-11-03 17:06:24 -0500

    modified script1 again

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #creating dir1 and file10 inside the dir1
-mkdir ~/hw4_data/dir1 && touch ~/hw4_data/dir1/file10
+mkdir dir1 && touch dir1/file10
 #creating dir2 and file20 inside the dir2
-mkdir ~/hw4_data/dir2 && touch ~/hw4_data/dir2/file20
+mkdir dir2 && touch dir2/file20
 #creating file 1
-touch ~/hw4_data/file1
+touch file1
 #soft link to file20
-ln -s ~/hw4_data/dir2/file20 ~/hw4_data/link1
+ln -s dir2/file20 link1
